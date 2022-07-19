2016f6eae239bf696d9bcd2b27e4483a1843524a
schedrov <rocolusso@icloud.com>
2022-07-19 18:04:18 +0300

    rewrited

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
 'use strict';
 /*
 Вам нужно написать функцию, которая в качестве параметра принимает
-функцию и добавляет ей возможность кешировать вызовы. Идея состоит в том,
-что при вызове функции с одинаковыми аргументами, нет смысла вызывать функцию каждый раз,
+функцию и добавляет ей возможность кешировать вызовы.
+
+Идея состоит в том,что при вызове функции с одинаковыми аргументами, нет смысла вызывать функцию каждый раз,
 достаточно сохранять данные о результатах вызова.
 
 Хранить нужно последние 10 вызовов.
  */
 
 void function (){
-    const callBack = () => {
-        return {call:'call'}
+    const callBack = (num) => {
+        return num;
     }
+    const map = new Map();
+    const arr = [];
     const func = (callback) => {
-        if (!localStorage.getItem('FunctionCache')){
-            let callsCount = 0;
-            const funcCallsCache = new Map();
-            callsCount ++;
-            funcCallsCache.set(callback, {calls:callsCount});
-            localStorage.setItem('FunctionCache', JSON.stringify(funcCallsCache.get(callback)));
-        } else {
-            let getCount = JSON.parse(localStorage.getItem('FunctionCache')).calls;
-            console.log(getCount)
-            if (getCount < 10){
-                const funcCallsCache = new Map();
-                getCount ++;
-                funcCallsCache.set(callback, {calls:getCount});
-                localStorage.setItem('FunctionCache', JSON.stringify(funcCallsCache.get(callback)));
-            } else {
-                localStorage.clear();
-            }
+        if (map.size >= 10) {
+           const arrFirstElem = arr[0];
+           map.delete(arrFirstElem);
+           arr.shift();
         }
-
+        arr.push(callback);
+        arr.forEach(item => {
+            map.set(item, item);
+        })
     }
-    func(callBack());
+
+    func(callBack(1));
+    func(callBack(2));
+    func(callBack(3));
+    func(callBack(4));
+    func(callBack(5));
+    func(callBack(5));
+    func(callBack(5));
+    func(callBack(5));
+    func(callBack(6));
+    func(callBack(7));
+    func(callBack(8));
+    func(callBack(9));
+    func(callBack(10));
+    func(callBack(11));
+    func(callBack(12));
+    func(callBack(13));
+    console.log(map)
+    console.log(arr)
 }();
 
